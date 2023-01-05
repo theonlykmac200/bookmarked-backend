edf16068acbc63e552fd9d381eff70dbe075b9bb
cps97 <colinstoutt@gmail.com>
2023-01-04 20:14:10 -0800

    added controller

---
+++ controller/bookMark.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bookMarkRouter = express.Router();
+const BookMark = require("../models/bookMark");
+
+bookMarkRouter.get("/", async (req, res) => {
+  try {
+    res.status(200).json(await BookMark.find({}));
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+bookMarkRouter.delete("/:id", async (req, res) => {
+  try {
+    res.status(200).json(await BookMark.findByIdAndDelete(req.params.id));
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+bookMarkRouter.put("/:id", async (req, res) => {
+  try {
+    res
+      .status(200)
+      .json(
+        await BookMark.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      );
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+bookMarkRouter.post("/", async (req, res) => {
+  try {
+    res.status(200).json(await BookMark.create(req.body));
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = bookMarkRouter;
--- server.js
+++ server.js
@@ -1,32 +1,39 @@
 // Dependencies
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
 
-const PORT = process.env.PORT
-const methodOverride = require('method-override')
-const mongoose = require('mongoose')
+const bookMarkController = require("./controller/bookMark");
+
+const PORT = process.env.PORT;
+const methodOverride = require("method-override");
+
+const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 // MIDDLEWARE
 // Body Parser middleware - give us access to req.body
-app.use(express.urlencoded({extended: false}))
-app.use(methodOverride('_method'))
-app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use("/static", express.static("public"));
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
 // Database Connection Logs
-const db = mongoose.connection
-db.on("error", (err) => console.log(err.message))
-db.on("connected", () => console.log("mongo connected"))
-db.on("disconnected", () => console.log("mongo disconnected"))
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err.message));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
-
-
-//INDEX
-
-// NEW
-
+app.use("/bookMarks", bookMarkController);
 
 // Listener
-app.listen(PORT, ()=> console.log(`You are listening to the smoothe sounds of port ${PORT}...`))
+app.listen(PORT, () =>
+  console.log(`You are listening to the smoothe sounds of port ${PORT}...`)
+);
